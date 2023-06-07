b774589cc3c2066826e57dd41081914d4de5ec09
arielpereyra1994 <135284169+arielpereyra1994@users.noreply.github.com>
2023-06-06 23:31:13 -0300

    Add files via upload

---
+++ js/miarchivo.js
@@ -0,0 +1,36 @@
+// let numero = prompt("Ingrese un numero");
+// let multiplos = 0
+
+// function tabla(){
+//     for (let i = 1; i <=10 ; i++){
+//         multiplos = numero * i;
+        
+//         alert(multiplos)
+        
+
+
+//     } 
+//     if (numero === 0){
+//         alert("El numero ingresado no es correcto");
+//     } else {
+        
+//     }
+// }
+// tabla()
+
+
+//Aca se crea la funcion
+function tabla() {
+    //Aca se solicita el numero del que se va a crear la tabla de multiplicar
+    numero = parseInt(prompt("Ingrese un número para hacer la tabla (0 para terminar):"));
+    //Aca se aplica el condicional(El numero tiene que ser distinto a 0)
+    while (numero !==0) {
+        for (var i = 1; i <= 10; i++) {
+        var resultado = numero * i;
+        document.write("<tr><td>" + numero + " x " + i + "</td><td>   =   " + resultado + "</td></tr>" + "<br>");
+        }
+        document.write("<br>"+"<br>"+"<br>");
+        numero = parseInt(prompt("Ingrese otro número (0 para terminar):"));
+    }
+}
+tabla()
