9dad8cd3a3dc9b5bd0d0d0152d0271216c38c105
arielpereyra1994 <135284169+arielpereyra1994@users.noreply.github.com>
2023-06-13 23:47:14 -0300

    Update miarchivo.js
    Modificacion del archivo original

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,16 +1,19 @@
-
 //Aca se crea la funcion
 function tabla() {
     //Aca se solicita el numero del que se va a crear la tabla de multiplicar
-    numero = parseInt(prompt("Ingrese un número para hacer la tabla (0 para terminar):"));
+    let numero = parseInt(prompt("Ingrese un número para hacer la tabla (0 para terminar):"));
+    
     //Aca se aplica el condicional(El numero tiene que ser distinto a 0)
-    while (numero !==0) {
-        for (var i = 1; i <= 10; i++) {
-        var resultado = numero * i;
+     while (numero !==0) {
+        for (let i = 1; i <= 10; i++) {
+        let resultado = numero * i;
         document.write("<tr><td>" + numero + " x " + i + "</td><td>   =   " + resultado + "</td></tr>" + "<br>");
-        }
+        }      
         document.write("<br>"+"<br>"+"<br>");
         numero = parseInt(prompt("Ingrese otro número (0 para terminar):"));
-    }
+        if (numero === 0){
+             alert("ERROR");
+        }
+     }
 }
 tabla()
